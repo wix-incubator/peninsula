3e4c9d1f4da1e34b3d9007141d209b6cbe0f8d5a
Valdemaras Repšys <valdemaras@wix.com>
2016-06-16 11:52:32 +0300

    Remove unnecessary test

--- src/test/scala/com/wix/peninsula/CopyConfigTest.scala
+++ src/test/scala/com/wix/peninsula/CopyConfigTest.scala
@@ -1,7 +1,5 @@
 package com.wix.peninsula
 
-import com.wix.peninsula.JsonValidators.NonEmptyStringValidator
-import org.json4s.JsonAST.JString
 import org.specs2.mutable.SpecificationWithJUnit
 
 class CopyConfigTest extends SpecificationWithJUnit {
@@ -25,47 +23,6 @@ class CopyConfigTest extends SpecificationWithJUnit {
       val json = Json.parse("""{"a": "b"}""")
       IdentityMapper.map(json) must_== json
     }
-  }
-
-
-
-  "random test" in {
-
-    import CopyConfigFactory._
-
-    object HttpsAppender extends JsonMapper {
-      override def map(json: Json): Json = Json(json.node match {
-        case JString(url) => JString("https:" + url)
-        case x => x
-      })
-    }
-
-    val config = TransformationConfig()
-      .add(copyField("id"))
-      .add(mergeObject("texts"))
-      .add(copyField("images.top" -> "media.pictures.headerBackground")
-        .withValidators(NonEmptyStringValidator)
-        .withMapper(HttpsAppender))
-
-    val json = Json.parse(
-      """
-	{
-	   "id":"1",
-	   "slug":"raw-metal",
-	   "name":"Raw Metal Gym",
-		 "texts": {
-			 "name": "Raw metal gym",
-			 "description": "The best gym in town. Come and visit us today!"
-		 },
-	   "images":{
-	      "top":"//images/top.jpg",
-	      "background":"//images/background.png"
-	   }
-	}
-      	""")
-
-    json.transform(config) must_== 1
-
   }
 
 }
