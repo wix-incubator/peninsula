37262178bc0c39eda8086715f1ffb0027767a755
Valdemaras Repšys <valdemaras@wix.com>
2017-03-14 16:30:47 +0200

    Fix string interpolation

--- src/main/scala/com/wix/peninsula/exceptions/JsonPathDoesntExistException.scala
+++ src/main/scala/com/wix/peninsula/exceptions/JsonPathDoesntExistException.scala
@@ -1,7 +1,4 @@
 package com.wix.peninsula.exceptions
 
-/**
-  * @author Ivan V Kamenev <ivanka@wix.com>
-  */
 case class JsonPathDoesntExistException(path: String)
-  extends RuntimeException("JSON element represented by path '$path' doesn't exist")
+  extends RuntimeException(s"JSON element represented by path '$path' doesn't exist")
