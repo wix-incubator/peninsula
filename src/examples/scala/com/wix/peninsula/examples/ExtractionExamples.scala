16fcd03917dab68981ca06216adcc11120b6d418
Ivan V Kamenev <ivanka@wix.com>
2017-03-15 17:18:19 +0200

    Added examples for extractStringOpt

--- src/examples/scala/com/wix/peninsula/examples/ExtractionExamples.scala
+++ src/examples/scala/com/wix/peninsula/examples/ExtractionExamples.scala
@@ -2,7 +2,9 @@ package com.wix.peninsula.examples
 
 import com.wix.peninsula.Json
 
-object ExtractionExamples extends App {
+trait ExtractionExamples
+
+object WhoIsToBlame extends App {
 
   val jsonStr =
     """
@@ -39,5 +41,49 @@ object ExtractionExamples extends App {
     println(s"Who is to blame: $responsiblePerson")
     println(s"What exactly happened: $errorsDescriptions")
   }
+
+}
+
+object MercedesFrom80s extends App {
+
+  val jsonStr =
+    """
+      {
+        "wix_users": [
+          {
+            "id": "uuid1",
+            "name": "Avishai"
+          },
+          {
+            "id": 2,
+            "name": "Pwned by G0Dandy"
+          },
+          {
+            "id": true,
+            "name": "Pwned by WorldPressure"
+          },
+          {
+            "id": null,
+            "name": "Pwned by Vovan the Freelancer"
+          },
+          {
+            "id": {";": "truncate table 'wix_users'"},
+            "name": "Sincerely yours, DBA"
+          },
+          {
+            "name": "Random guy without id"
+          }
+        ]
+      }
+    """
+
+  val json = Json.parse(jsonStr)
+
+  println(""""uuid1" is extracted as """ + json.extractStringOpt("wix_users(0).id"))
+  println("""2 is extracted as """ + json.extractStringOpt("wix_users(1).id"))
+  println("""true is extracted as """ + json.extractStringOpt("wix_users(2).id"))
+  println("""null is extracted as """ + json.extractStringOpt("wix_users(3).id"))
+  println("""{";": "truncate table 'users'"} is extracted as """ + json.extractStringOpt("wix_users(4).id"))
+  println("""nonexistent element is extracted as """ + json.extractStringOpt("wix_users(5).id"))
 
 }
